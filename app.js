d83b23f61298b51161c281de5c1deec4dbab5e93
Louis Williams <louawilliams@outlook.com>
2020-07-06 20:55:41 -0400

    Passport Facebook

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const overviewRouter = require('./routes/overviewRouter');
+const createError = require('http-errors');
+const path = require('path');
+const logger = require('morgan');
+const passport = require('passport');
+const config = require('./config');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const transactionRouter = require('./routes/transactionRouter');
 const billRouter = require('./routes/billRouter');
 const budgetRouter = require('./routes/budgetRouter');
 const goalRouter = require('./routes/goalRouter');
 const investmentRouter = require('./routes/investmentRouter');
 
-const hostname = 'localhost';
-const port = 3000;
-
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/poundwiseServer';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
     useFindAndModify: false,
@@ -26,26 +28,51 @@ connect.then(() => console.log('Connected correctly to server'),
 );
 
 const app = express();
-app.use(morgan('dev'));
-app.use(bodyParser.json());
 
-app.use('/overview', overviewRouter);
+// Secure traffic only
+app.all('*', (req, res, next) => {
+    if (req.secure) {
+      return next();
+    } else {
+        console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+        res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+    }
+  });
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');  
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/index', indexRouter);
 app.use('/transactions', transactionRouter);
 app.use('/bills', billRouter);
 app.use('/budgets', budgetRouter);
 app.use('/goals', goalRouter);
 app.use('/investments', investmentRouter);
 
-app.use(express.static(__dirname + '/public'));
-
-app.use((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-});
-
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
 module.exports = app;
---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/poundwiseserver',
+    'facebook': {
+        clientId: '612378206065639',
+        clientSecret: 'e8dfccf13954898b1844f0b12e56b64c'
+    }
+}
