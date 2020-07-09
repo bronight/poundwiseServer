700884e4d3ebbf1e752dfeab67dd01e7702d9880
Louis Williams <louawilliams@outlook.com>
2020-07-08 22:43:36 -0400

    Removed const user

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ const express = require('express');
 const User = require('../models/user');
 const passport = require('passport');
 const authenticate = require('../authenticate');
-const user = require('../models/user');
 const cors = require('./cors');
 
 const router = express.Router();
